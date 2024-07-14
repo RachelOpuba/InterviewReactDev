6322d9907211a2095137c95feb6b67f7d50075f1
Rachel Opuba <ogbenirach@gmail.com>
2024-07-14 17:03:49 +0100

    Added padding to modal heading

--- src/components/ImageModal.jsx
+++ src/components/ImageModal.jsx
@@ -4,8 +4,8 @@ function ImageModal({ images, onClose }) {
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
       <div className="bg-white rounded-lg shadow-lg p-4 max-w-3xl w-full overflow-y-auto max-h-screen">
-        <div className="flex justify-between items-center mb-4">
-          <h2 className="text-2xl font-semibold pb-4">Product Images</h2>
+        <div className="flex justify-between items-center mb-4 pt-6 ps-4">
+          <h2 className="text-2xl font-semibold pb-4 ">Product Images</h2>
           <button
             onClick={onClose}
             className="text-red-500 hover:text-red-700 text-[40px]"
